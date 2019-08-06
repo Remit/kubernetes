1cd1105b6514786494d2a2fd9d24a064eac6a904
Remit <v.e.podolskiy@gmail.com>
2019-08-06 20:34:30 +0200

    started with cloudsuite

---
+++ benchmarks/cloudsuite/data-analytics/start-benchmark.sh
@@ -0,0 +1,4 @@
+# https://docs.projectcalico.org/v1.6/getting-started/docker/tutorials/basic
+# Calico network for container? to connect to calico for pods?
+# https://kubernetes.io/docs/concepts/services-networking/network-policies/
+# https://docs.projectcalico.org/v3.8/security/calico-network-policy
