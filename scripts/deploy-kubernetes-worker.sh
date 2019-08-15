5f65b4708816cfbef33c2a6a6373d92fcb90d2c7
Remit <v.e.podolskiy@gmail.com>
2019-08-16 00:17:50 +0200

    d

--- scripts/deploy-kubernetes-worker.sh
+++ scripts/deploy-kubernetes-worker.sh
@@ -24,9 +24,9 @@ if [ ! -z "$3" ]
 fi
 
 # If CNI configuration does not work - check https://docs.projectcalico.org/v1.5/getting-started/kubernetes/installation/
-sudo rm -rf /var/lib/cni/
+# sudo rm -rf /var/lib/cni/
 sudo kubeadm join $1 --token $2 --discovery-token-ca-cert-hash $3
-sudo kubectl apply -f https://docs.projectcalico.org/v3.8/manifests/calico.yaml
+# sudo kubectl apply -f https://docs.projectcalico.org/v3.8/manifests/calico.yaml
 # In case of issues with Calico - https://github.com/projectcalico/calico/issues/2699
 # Here we enable some ports to be used when exposing pod ports via services for convenience
 sudo cat /etc/kubernetes/manifests/kube-apiserver.yaml | sed 's/- --secure-port=6443/- --secure-port=6443\n    - --service-node-port-range=8000-60000/' > kube-apiserver.yaml
